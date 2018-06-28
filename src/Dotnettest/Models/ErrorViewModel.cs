ed9f80608e26b7ce8780083b5698ae3431dc341c
Vidar Kongsli <vidar@kongs.li>
2018-06-28 07:34:59 +0200

    Add code from course

---
+++ src/Dotnettest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dotnettest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
